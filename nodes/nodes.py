71651bb9f04c45a800c98a10e218cf0f9e035bfa
Akatsuzi <42118269+Suzie1@users.noreply.github.com>
2023-10-29 09:53:46 +0000

    Add files via upload
    replaced file

---
+++ nodes/nodes.py
@@ -0,0 +1,390 @@
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# Comfyroll Custom Nodes by RockOfFire and Akatsuzi         https://github.com/RockOfFire/ComfyUI_Comfyroll_CustomNodes                             #
+# for ComfyUI                                               https://github.com/comfyanonymous/ComfyUI                                               #
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+
+import torch
+import numpy as np
+import os
+import sys
+import io
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+import json
+import folder_paths
+import typing as tg
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#           
+# This node has a bunch of default picture widths and heights as well as being able to choose them yourself.
+class CR_AspectRatioSD15:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+    
+        aspect_ratios = ["custom", "1:1 square 512x512", "1:1 square 1024x1024",
+            "2:3 portrait 512x768", "3:4 portrait 512x682",
+            "3:2 landscape 768x512", "4:3 landscape 682x512",
+            "16:9 cinema 910x512", "2:1 cinema 1024x512"]
+               
+        return {
+            "required": {
+                "width": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                "height": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                "aspect_ratio": (aspect_ratios,),
+                "swap_dimensions": (["Off", "On"],),
+                "upscale_factor": ("FLOAT", {"default": 1, "min": 1, "max": 2000}),
+                "batch_size": ("INT", {"default": 1, "min": 1, "max": 64})
+            }
+        }
+    RETURN_TYPES = ("INT", "INT", "FLOAT", "INT")
+    RETURN_NAMES = ("width", "height", "upscale_factor", "batch_size")
+    FUNCTION = "Aspect_Ratio"
+    CATEGORY = "Comfyroll/Image"
+
+    def Aspect_Ratio(self, width, height, aspect_ratio, swap_dimensions, upscale_factor, batch_size):
+        if swap_dimensions == "Off":
+            if aspect_ratio == "2:3 portrait 512x768":
+                width, height = 512, 768
+            elif aspect_ratio == "3:2 landscape 768x512":
+                width, height = 768, 512
+            elif aspect_ratio == "1:1 square 512x512":
+                width, height = 512, 512
+            elif aspect_ratio == "1:1 square 1024x1024":
+                width, height = 1024, 1024
+            elif aspect_ratio == "16:9 cinema 910x512":
+                width, height = 910, 512
+            elif aspect_ratio == "3:4 portrait 512x682":
+                width, height = 512, 682
+            elif aspect_ratio == "4:3 landscape 682x512":
+                width, height = 682, 512
+            elif aspect_ratio == "2:1 cinema 1024x512":
+                width, height = 1024, 512
+            return(width, height, upscale_factor, batch_size)
+
+        if swap_dimensions == "On":
+            return(height, width, upscale_factor, batch_size,)
+        else:
+            return(width, height, upscale_factor, batch_size,)  
+            
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+#This node makes the upscale factor a float
+class CR_ImageSize:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "width": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                "height": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                "upscale_factor": ("FLOAT", {"default": 1, "min": 1, "max": 2000})
+            }
+        }
+    RETURN_TYPES = ("INT", "INT", "FLOAT")
+    RETURN_NAMES = ("Width", "Height", "upscale_factor")
+    FUNCTION = "ImageSize"
+    CATEGORY = "Comfyroll/Legacy"
+
+    def ImageSize(self, width, height, upscale_factor):
+        return(width, height, upscale_factor)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+
+#Legacy Node. This node was an attempt at making a save and preview image node into one.
+class CR_ImageOutput:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                    "output_type": (["Preview", "Save"],),
+                     "filename_prefix": ("STRING", {"default": "ComfyUI"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+                "optional": {
+                    "trigger": ("BOOLEAN", {"default": False},),}
+                }
+
+    RETURN_TYPES = ("BOOLEAN", )
+    RETURN_NAMES = ("trigger", )
+    FUNCTION = "save_images"
+    OUTPUT_NODE = True
+    CATEGORY = "Comfyroll/Legacy"
+
+    def save_images(self, images, filename_prefix="ComfyUI", trigger = False, output_type = "Preview", prompt=None, extra_pnginfo=None):
+        def map_filename(filename):
+            prefix_len = len(os.path.basename(filename_prefix))
+            prefix = filename[:prefix_len + 1]
+            try:
+                digits = int(filename[prefix_len + 1:].split('_')[0])
+            except:
+                digits = 0
+            return (digits, prefix)
+
+        def compute_vars(input):
+            input = input.replace("%width%", str(images[0].shape[1]))
+            input = input.replace("%height%", str(images[0].shape[0]))
+            return input
+
+        if output_type == "Save":
+            self.output_dir = folder_paths.get_output_directory()
+            self.type = "output"
+        elif output_type == "Preview":
+            self.output_dir = folder_paths.get_temp_directory()
+            self.type = "temp"
+
+        filename_prefix = compute_vars(filename_prefix)
+
+        subfolder = os.path.dirname(os.path.normpath(filename_prefix))
+        filename = os.path.basename(os.path.normpath(filename_prefix))
+
+        full_output_folder = os.path.join(self.output_dir, subfolder)
+
+        if os.path.commonpath((self.output_dir, os.path.abspath(full_output_folder))) != self.output_dir:
+            return {}
+
+        try:
+            counter = max(filter(lambda a: a[1][:-1] == filename and a[1][-1] == "_", map(map_filename, os.listdir(full_output_folder))))[0] + 1
+        except ValueError:
+            counter = 1
+        except FileNotFoundError:
+            os.makedirs(full_output_folder, exist_ok=True)
+            counter = 1
+
+        results = list()
+        for image in images:
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = PngInfo()
+            if prompt is not None:
+                metadata.add_text("prompt", json.dumps(prompt))
+            if extra_pnginfo is not None:
+                for x in extra_pnginfo:
+                    metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+
+            file = f"{filename}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=4)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results }, "result": (trigger,) }
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# This node will give you the multiple of the value within the interger parameter.
+class CR_IntegerMultipleOf:
+    def __init__(self):
+        pass
+        
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "integer": ("INT", {"default": 1, "min": -18446744073709551615, "max": 18446744073709551615}),
+                "multiple": ("FLOAT", {"default": 8, "min": 1, "max": 18446744073709551615}),
+            }
+        }
+    
+    RETURN_TYPES =("INT",)
+    FUNCTION = "int_multiple_of"    
+    CATEGORY = "Comfyroll/Math"
+    
+    def int_multiple_of(self, integer, multiple=8):
+        if multiple == 0:
+            return (int(integer), )
+        integer = integer * multiple
+        return (int(integer), )
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# This node is for making seeds
+class CR_Seed:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                },
+        }
+
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("seed",)
+    FUNCTION = "seedint"
+    OUTPUT_NODE = True
+    CATEGORY = "Comfyroll/Number"
+
+    @staticmethod
+    def seedint(seed):
+        return seed,
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# Based on Color Tint node by hnmr293
+# Adds a color tint into the picture
+class CR_ColorTint:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+    
+        modes = ["white", "black", "sepia", "red", "green", "blue",
+            "cyan", "magenta", "yellow", "purple", "orange", "warm",
+            "cool",  "lime", "navy", "vintage", "rose", "teal",
+            "maroon", "peach", "lavender", "olive"]
+            
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "strength": ("FLOAT", {"default": 1.0,"min": 0.1,"max": 1.0,"step": 0.1}),
+                "mode": (modes,),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "color_tint"
+    CATEGORY = "Comfyroll/Image"
+
+    def color_tint(self, image: torch.Tensor, strength: float, mode: str = "sepia"):
+        if strength == 0:
+            return (image,)
+
+        sepia_weights = torch.tensor([0.2989, 0.5870, 0.1140]).view(1, 1, 1, 3).to(image.device)
+      
+        mode_filters = {
+            "white": torch.tensor([1.0, 1.0, 1.0]),
+            "black": torch.tensor([0, 0, 0]),
+            "sepia": torch.tensor([1.0, 0.8, 0.6]),
+            "red": torch.tensor([1.0, 0.6, 0.6]),
+            "green": torch.tensor([0.6, 1.0, 0.6]),
+            "blue": torch.tensor([0.6, 0.8, 1.0]),
+            "cyan": torch.tensor([0.6, 1.0, 1.0]),
+            "magenta": torch.tensor([1.0, 0.6, 1.0]),
+            "yellow": torch.tensor([1.0, 1.0, 0.6]),
+            "purple": torch.tensor([0.8, 0.6, 1.0]),
+            "orange": torch.tensor([1.0, 0.7, 0.3]),
+            "warm": torch.tensor([1.0, 0.9, 0.7]),
+            "cool": torch.tensor([0.7, 0.9, 1.0]),
+            "lime": torch.tensor([0.7, 1.0, 0.3]),
+            "navy": torch.tensor([0.3, 0.4, 0.7]),
+            "vintage": torch.tensor([0.9, 0.85, 0.7]),
+            "rose": torch.tensor([1.0, 0.8, 0.9]),
+            "teal": torch.tensor([0.3, 0.8, 0.8]),
+            "maroon": torch.tensor([0.7, 0.3, 0.5]),
+            "peach": torch.tensor([1.0, 0.8, 0.6]),
+            "lavender": torch.tensor([0.8, 0.6, 1.0]),
+            "olive": torch.tensor([0.6, 0.7, 0.4]),
+        }
+
+        scale_filter = mode_filters[mode].view(1, 1, 1, 3).to(image.device)
+
+        grayscale = torch.sum(image * sepia_weights, dim=-1, keepdim=True)
+        tinted = grayscale * scale_filter
+
+        result = tinted * strength + image * (1 - strength)
+        return (result,)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# Sets the latent batch size
+class CR_LatentBatchSize:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "latent": ("LATENT", ),
+                "batch_size": ("INT", {"default": 2, "min": 1, "max": 16, "step": 1}),
+            },
+        }
+
+    RETURN_TYPES = ("LATENT", )
+    FUNCTION = "batchsize"
+    CATEGORY = "Comfyroll/Latent"
+
+    def batchsize(self, latent: tg.Sequence[tg.Mapping[tg.Text, torch.Tensor]], batch_size: int):
+        samples = latent['samples']
+        shape = samples.shape
+
+        sample_list = [samples] + [
+            torch.clone(samples) for _ in range(batch_size - 1)
+        ]
+
+        return ({
+            'samples': torch.cat(sample_list),
+        }, )
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+#Text for prompts
+class CR_PromptText:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+                    "prompt": ("STRING", {"default": "prompt", "multiline": True}),
+                    },
+                }
+
+    RETURN_TYPES = ("STRING", )
+    RETURN_NAMES = ("prompt", )
+    FUNCTION = "get_value"
+    CATEGORY = 'Comfyroll/Text'
+
+    def get_value(self, prompt):
+        return (prompt,)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+class CR_SplitString:
+
+    @classmethod
+    def INPUT_TYPES(s):  
+    
+        return {"required": {"text": ("STRING", {"multiline": False, "default": "text"}),
+                             "delimiter": ("STRING", {"multiline": False, "default": ","}), 
+                },
+        }
+
+    RETURN_TYPES = ("STRING", "STRING", "STRING", "STRING",)
+    RETURN_NAMES = ("string_1", "string_2", "string_3", "string_4",)    
+    FUNCTION = "split"
+    CATEGORY = "Comfyroll/Text"
+
+    def split(self, text, delimiter):
+
+        # Split the text string
+        parts = text.split(delimiter)
+        strings = [part.strip() for part in parts[:4]]
+        string_1, string_2, string_3, string_4 = strings + [""] * (4 - len(strings))            
+
+        return (string_1, string_2, string_3, string_4)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# MAPPINGS
+#---------------------------------------------------------------------------------------------------------------------------------------------------#
+# For reference only, actual mappings are in __init__.py
+'''
+NODE_CLASS_MAPPINGS = {
+    "CR Image Size": CR_ImageSize,
+    "CR Image Output": CR_ImageOutput,
+    "CR Integer Multiple": CR_IntegerMultipleOf,
+    "CR SD1.5 Aspect Ratio": CR_AspectRatioSD15,
+    "CR Color Tint": CR_ColorTint,
+    "CR Latent Batch Size":CR_LatentBatchSize
+    "CR Seed":CR_Seed,
+    "CR Prompt Text":CR_PromptText,
+    "CR Split String":CR_SplitString, 
+}
+'''
+
