ee1c8b3acc68471e05bd7073915e66bf6a13e342
Akatsuzi <42118269+Suzie1@users.noreply.github.com>
2023-11-07 09:10:23 -0600

    Update nodes.py
    move Color Tint

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -213,72 +213,6 @@ class CR_Seed:
         return seed,
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------#
-# Based on Color Tint node by hnmr293
-# Adds a color tint into the picture
-class CR_ColorTint:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-    
-        modes = ["white", "black", "sepia", "red", "green", "blue",
-            "cyan", "magenta", "yellow", "purple", "orange", "warm",
-            "cool",  "lime", "navy", "vintage", "rose", "teal",
-            "maroon", "peach", "lavender", "olive"]
-            
-        return {
-            "required": {
-                "image": ("IMAGE",),
-                "strength": ("FLOAT", {"default": 1.0,"min": 0.1,"max": 1.0,"step": 0.1}),
-                "mode": (modes,),
-            },
-        }
-
-    RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "color_tint"
-    CATEGORY = CATEGORY = icons.get("Comfyroll/Graphics/Filter")
-
-    def color_tint(self, image: torch.Tensor, strength: float, mode: str = "sepia"):
-        if strength == 0:
-            return (image,)
-
-        sepia_weights = torch.tensor([0.2989, 0.5870, 0.1140]).view(1, 1, 1, 3).to(image.device)
-      
-        mode_filters = {
-            "white": torch.tensor([1.0, 1.0, 1.0]),
-            "black": torch.tensor([0, 0, 0]),
-            "sepia": torch.tensor([1.0, 0.8, 0.6]),
-            "red": torch.tensor([1.0, 0.6, 0.6]),
-            "green": torch.tensor([0.6, 1.0, 0.6]),
-            "blue": torch.tensor([0.6, 0.8, 1.0]),
-            "cyan": torch.tensor([0.6, 1.0, 1.0]),
-            "magenta": torch.tensor([1.0, 0.6, 1.0]),
-            "yellow": torch.tensor([1.0, 1.0, 0.6]),
-            "purple": torch.tensor([0.8, 0.6, 1.0]),
-            "orange": torch.tensor([1.0, 0.7, 0.3]),
-            "warm": torch.tensor([1.0, 0.9, 0.7]),
-            "cool": torch.tensor([0.7, 0.9, 1.0]),
-            "lime": torch.tensor([0.7, 1.0, 0.3]),
-            "navy": torch.tensor([0.3, 0.4, 0.7]),
-            "vintage": torch.tensor([0.9, 0.85, 0.7]),
-            "rose": torch.tensor([1.0, 0.8, 0.9]),
-            "teal": torch.tensor([0.3, 0.8, 0.8]),
-            "maroon": torch.tensor([0.7, 0.3, 0.5]),
-            "peach": torch.tensor([1.0, 0.8, 0.6]),
-            "lavender": torch.tensor([0.8, 0.6, 1.0]),
-            "olive": torch.tensor([0.6, 0.7, 0.4]),
-        }
-
-        scale_filter = mode_filters[mode].view(1, 1, 1, 3).to(image.device)
-
-        grayscale = torch.sum(image * sepia_weights, dim=-1, keepdim=True)
-        tinted = grayscale * scale_filter
-
-        result = tinted * strength + image * (1 - strength)
-        return (result,)
-
-#---------------------------------------------------------------------------------------------------------------------------------------------------#
 # Sets the latent batch size
 class CR_LatentBatchSize:
 
@@ -379,7 +313,6 @@ NODE_CLASS_MAPPINGS = {
     "CR Image Output": CR_ImageOutput,
     "CR Integer Multiple": CR_IntegerMultipleOf,
     "CR SD1.5 Aspect Ratio": CR_AspectRatioSD15,
-    "CR Color Tint": CR_ColorTint,
     "CR Latent Batch Size":CR_LatentBatchSize
     "CR Seed":CR_Seed,
     "CR Prompt Text":CR_PromptText,
