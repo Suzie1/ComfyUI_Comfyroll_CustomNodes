803747a95279ff08278a8df8e9118aab5b8d5140
Akatsuzi <42118269+Suzie1@users.noreply.github.com>
2023-11-07 10:12:25 -0600

    Update pil_text.py
    added Arabic Text RTL node

--- nodes/pil_text.py
+++ nodes/pil_text.py
@@ -15,6 +15,18 @@ from .graphics_functions import (draw_masked_text,
                                  get_font_size,
                                  get_color_values)
 
+try:
+    from bidi.algorithm import get_display
+except ImportError:
+    import subprocess
+    subprocess.check_call(['python', '-m', 'pip', 'install', 'python_bidi'])
+
+try:    
+    import arabic_reshaper    
+except ImportError:
+    import subprocess
+    subprocess.check_call(['python', '-m', 'pip', 'install', 'arabic_reshaper'])
+  
 font_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "fonts")       
 file_list = [f for f in os.listdir(font_dir) if os.path.isfile(os.path.join(font_dir, f)) and f.lower().endswith(".ttf")]
 
@@ -276,7 +288,41 @@ class CR_CompositeText:
         
         # Convert the PIL image back to a torch tensor
         return pil2tensor(image_out),
- 
+
+#---------------------------------------------------------------------------------------------------------------------#
+class CR_ArabicTextRTL:
+    
+    @classmethod
+    def INPUT_TYPES(s):
+                        
+        return {"required": {
+                "arabic_text": ("STRING", {"multiline": True, "default": "شمس"}),
+                }          
+        }
+
+    RETURN_TYPES = ("STRING", "STRING", )
+    RETURN_NAMES = ("arabic_text_rtl", "show help", )
+    FUNCTION = "adjust_arabic_to_rtl"
+    CATEGORY = icons.get("Comfyroll/Graphics/Text")
+
+    def adjust_arabic_to_rtl(self, arabic_text):
+        """
+        Adjust Arabic text to read from right to left (RTL).
+        
+        Args:
+            arabic_text (str): The Arabic text to be adjusted.
+            
+        Returns:
+            str: The adjusted Arabic text in RTL format.
+        """
+        
+        arabic_text_reshaped = arabic_reshaper.reshape(arabic_text)
+        rtl_text = get_display(arabic_text_reshaped)
+        
+        show_help = "example help text"
+                
+        return rtl_text, show_help,
+
 #---------------------------------------------------------------------------------------------------------------------#
 # MAPPINGS
 #---------------------------------------------------------------------------------------------------------------------#
@@ -288,6 +334,7 @@ NODE_CLASS_MAPPINGS = {
     "CR Mask Text":CR_MaskText,
     "CR Composite Text":CR_CompositeText,
     "CR Draw Perspective Text":CR_DrawPerspectiveText,
+    "CR Arabic Text RTL": CR_ArabicTextRTL,
 }
 '''
 
