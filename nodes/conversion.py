9d1123d90b713ed393dc72bb451e803ace63ed84
rbfussell <rbfussell@gmail.com>
2023-12-04 22:45:06 -0600

    Fixed duplicate RETURN_TYPES def

--- nodes/conversion.py
+++ nodes/conversion.py
@@ -71,7 +71,6 @@ class CR_StringToCombo:
             },
         }
 
-    RETURN_TYPES = (any, "STRING", )
     RETURN_TYPES = ("any", "show_help", )
     FUNCTION = "convert"
     CATEGORY = icons.get("Comfyroll/Utils/Conversion")
